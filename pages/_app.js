67c1f690590b6a0604ad27c7ee21d1cbd9cf5111
BulaSete <36999228+xkatld@users.noreply.github.com>
2024-10-09 11:32:08 +0800

    Create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect } from 'react';
+
+function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    import('bootstrap/dist/js/bootstrap');
+  }, []);
+
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
